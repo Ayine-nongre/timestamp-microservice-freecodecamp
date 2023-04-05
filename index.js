2067919293bb4e150658fc66c9b366b9a6392dc5
Ayine-nongre <eugeneatinbire@gmail.com>
2023-04-05 10:50:45 +0000

    Adding a working timestamp api microservice

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -12,6 +13,7 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -24,9 +26,34 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function (req, res){
+  const date = new Date().toUTCString();
+  const unix = Date.parse(date);
+
+  res.json({unix: unix, utc: date});
+})
+
+app.get("/api/:date", function (req, res){
+  let dateString = req.params.date;
+  let pattern = /^[0-9]+$/;
+  let dateType = pattern.test(dateString);
+
+  if (dateType){
+    const unixTimeStamp = parseInt(dateString);
+    const utc = new Date(unixTimeStamp).toUTCString();
+  
+    res.json({unix: unixTimeStamp, utc: utc})
+  }else{
+    const unixTimestamp = Date.parse(dateString);
+    const utc = new Date(unixTimestamp).toUTCString();
+
+    unixTimestamp ? res.json({ unix: unixTimestamp, utc: utc }) : res.json({ error: "Invalid Date" })
+  }
+  console.log(dateType);
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
